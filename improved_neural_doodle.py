b80547b71151ead787d705a19e0c35571f6f6314
Somshubra Majumdar <titu1994@gmail.com>
2016-09-10 23:49:40 +0530

    Added min improvement

--- improved_neural_doodle.py
+++ improved_neural_doodle.py
@@ -43,7 +43,8 @@ parser.add_argument("--img_size", type=int, default=-1, help='Image size will be
 parser.add_argument("--num_iter", dest="num_iter", default=10, type=int, help="Number of iterations")
 parser.add_argument('--preserve_color', dest='color', default="False", type=str,
                     help='Preserve original color in image')
-parser.add_argument("--phases", default=1, type=int, help='Number of phases to pass through')
+parser.add_argument("--min_improvement", default=0.0, type=float,
+                    help="Minimum improvement required to continue training")
 
 parser.add_argument("--content_weight", dest="content_weight", default=0.1, type=float, help="Weight of content")
 parser.add_argument("--style_weight", dest="style_weight", default=1, type=float, help="Weight of content")
@@ -387,3 +388,9 @@ for i in range(args.num_iter):
     end_time = time.time()
     print('Image saved as', fname)
     print('Iteration %d completed in %ds' % (i + 1, end_time - start_time))
+
+    if args.min_improvement != 0.0:
+        if improvement < args.min_improvement:
+            print("Script is early stopping since improvement (%0.2f) < min improvement (%0.2f)" %
+                  (improvement, args.min_improvement))
+            exit()
