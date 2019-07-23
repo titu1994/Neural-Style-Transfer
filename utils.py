93064f955c7c057caae92311e4897ba5f378ac05
Somshubra Majumdar <titu1994@gmail.com>
2019-07-23 11:32:44 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+import imageio
+from PIL import Image
+
+
+def imread(path, mode="RGB"):
+    img = np.array(imageio.imread(path, pilmode=mode))
+    return img
+    
+
+def imresize(img, size):
+    img = np.array(Image.fromarray(img).resize(size))
+    return img
+    
+    
+def imsave(path, img):
+    imageio.imwrite(path, img)
+    return
+    
+
+def fromimage(arr):
+    return np.array(arr)
+
+
+def toarray(arr):
+    return Image.fromarray(arr)
